5cabaac6907b3af09010c3bbeecb7642c865232b
ldm0 <liudingming@bytedance.com>
2025-09-23 13:53:55 +0800

    Use jemalloc

--- cardinal/src-tauri/src/main.rs
+++ cardinal/src-tauri/src/main.rs
@@ -1,6 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 fn main() {
     cardinal_lib::run().unwrap();
 }
